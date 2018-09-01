eed0f51a10acdbd7e87c4df5aec452a17d3c16a5
李進興 Vulcan <vulcan@miniasp.com>
2018-09-01 12:18:19 +0800

    使用 XAML 來進行跨平台行動應用程式開發

---
+++ XAMLCodeBehind/XAMLCodeBehind/XAMLCodeBehind/HomePage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XAMLCodeBehind
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class HomePage : ContentPage
+	{
+        int index = 0;
+        public HomePage ()
+		{
+			InitializeComponent ();
+		}
+
+        private void button_Clicked(object sender, EventArgs e)
+        {
+            label.Text = $"點選按鈕次數 {++index}";
+        }
+    }
+}
