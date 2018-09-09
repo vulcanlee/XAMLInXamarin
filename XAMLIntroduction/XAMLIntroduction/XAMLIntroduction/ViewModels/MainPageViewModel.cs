dbd6394bd9b100c587ccd90133fb53eca3022524
李進興 Vulcan <vulcan@miniasp.com>
2018-09-09 16:56:43 +0800

    XAML 語法與基本概念

---
+++ XAMLIntroduction/XAMLIntroduction/XAMLIntroduction/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XAMLIntroduction.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        public MainPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            Title = "XAML 基本用法介紹";
+        }
+    }
+}
