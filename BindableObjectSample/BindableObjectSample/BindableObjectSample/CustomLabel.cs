8aa5a7be05a0abff254f0ff0741bdf0bbd26d156
李進興 Vulcan <vulcan@miniasp.com>
2018-09-14 00:28:55 +0800

    綁定物件與綁定屬性範例

---
+++ BindableObjectSample/BindableObjectSample/BindableObjectSample/CustomLabel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace BindableObjectSample
+{
+    class CustomLabel : Label
+    {
+        #region CustomText 可綁定屬性 BindableProperty
+        public static readonly BindableProperty CustomTextProperty =
+            BindableProperty.Create("CustomText", // 屬性名稱 
+                typeof(string), // 回傳類型 
+                typeof(CustomLabel), // 宣告類型 
+                "", // 預設值 
+                defaultBindingMode: BindingMode.TwoWay,
+                propertyChanged: OnCustomTextChanged  // 屬性值異動時，要執行的事件委派方法
+            );
+
+        public string CustomText
+        {
+            set
+            {
+                SetValue(CustomTextProperty, value);
+            }
+            get
+            {
+                return (string)GetValue(CustomTextProperty);
+            }
+        }
+
+        private static void OnCustomTextChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+        }
+
+        #endregion
+
+    }
+}
---
+++ BindableObjectSample/BindableObjectSample/BindableObjectSample/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BindableObjectSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            this.PropertyChanging += DoPropertyChanged;
+            
+        }
+
+        private void DoPropertyChanged(object sender, Xamarin.Forms.PropertyChangingEventArgs e)
+        {
+            Console.WriteLine($"==>{e.PropertyName}");
+        }
+
+        private void btnSetValue_Clicked(object sender, EventArgs e)
+        {
+            label.SetValue(CustomLabel.CustomTextProperty, "呼叫 SetValue 變更屬性值");
+        }
+
+        private void btnDirect_Clicked(object sender, EventArgs e)
+        {
+            label.CustomText = "直接變更屬性值";
+        }
+
+        private void btnViewModel_Clicked(object sender, EventArgs e)
+        {
+            var fooVM = this.BindingContext as MainPageViewModel;
+            fooVM.Title = "透過 檢視模型 變更文字標籤文字內容";
+        }
+
+        //private void DoPropertyChanged(object sender, PropertyChangedEventArgs e)
+        //{
+        //    Console.WriteLine($"==>{e.PropertyName}");
+        //}
+
+    }
+}
