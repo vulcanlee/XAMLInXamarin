e787a34eb8601823a2669431b84cadc3b96de5a7
李進興 Vulcan <vulcan@miniasp.com>
2018-09-18 21:04:35 +0800

    使用 檢視 內提供的 LayoutOptions 的 對齊 / 擴充 設定選項 了解這兩個設定選項在 Xamarin.Forms 表現出來的效果 這裡是僅有使用 FillAndExpand ，請與單獨使用 Fill 的設定情境下做比較

---
+++ StackLayoutLab2/StackLayoutLab2/StackLayoutLab2/Sample2Page.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace StackLayoutLab2
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Sample2Page : ContentPage
+	{
+		public Sample2Page ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
