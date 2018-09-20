61173c774de5c96b020b10f31daafcf23d29be65
李進興 Vulcan <vulcan@miniasp.com>
2018-09-20 22:26:21 +0800

    VisualElement顏色使用範例

---
+++ VisualElementColor/VisualElementColor/VisualElementColor/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace VisualElementColor
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            labelDefault.Text = $"預設背景顏色，檢測 Background 屬性值是否可以繼承 {this.BackgroundColor.ToString()}";
+        }
+    }
+}
