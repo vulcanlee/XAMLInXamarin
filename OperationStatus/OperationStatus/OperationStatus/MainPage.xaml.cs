8f4b6a32c3b6a9f46d851410290dada3eaa5d182
李進興 Vulcan <vulcan@miniasp.com>
2018-09-21 12:26:27 +0800

    VisualElement 顯示與操作狀態 使用範例

---
+++ OperationStatus/OperationStatus/OperationStatus/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace OperationStatus
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonIsVisable_Clicked(object sender, EventArgs e)
+        {
+            boxview1.IsVisible = !boxview1.IsVisible;
+        }
+    }
+}
