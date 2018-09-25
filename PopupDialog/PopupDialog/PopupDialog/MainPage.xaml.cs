65e9626ca3d5b4922e025c0ca127a54d7a4ec023
李進興 Vulcan <vulcan@miniasp.com>
2018-09-25 20:24:18 +0800

    頁面之快顯示窗 使用範例

---
+++ PopupDialog/PopupDialog/PopupDialog/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PopupDialog
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnDisplayAlert_Clicked(object sender, EventArgs e)
+        {
+            bool result = await this.DisplayAlert("警告", "您確定要開始進行這個工作嗎？", "是的，我願意", "不，要取消");
+            label.Text = $"[DisplayAlert] 您的選擇結果是 {result}";
+        }
+
+        private async void btnDisplayActionSheet_Clicked(object sender, EventArgs e)
+        {
+            string result = await this.DisplayActionSheet("請選擇您的需求", "取消", null, "C#", "XAML", "MVVM", "Prism", "C# Async");
+            label.Text = $"[DisplayActionSheet] 您的選擇結果是 {result}";
+        }
+    }
+}
