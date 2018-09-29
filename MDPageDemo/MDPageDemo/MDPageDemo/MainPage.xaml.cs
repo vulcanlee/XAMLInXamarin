c6b9d18695a63f867c4ee9620ceeaa5330d9035a
李進興 Vulcan <vulcan@miniasp.com>
2018-09-29 18:46:16 +0800

    內建主從階層頁面樣板 使用範例

--- MDPageDemo/MDPageDemo/MDPageDemo/MainPage.xaml.cs
+++ MDPageDemo/MDPageDemo/MDPageDemo/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
@@ -13,16 +14,20 @@ namespace MDPageDemo
         {
             InitializeComponent();
 
+            Console.WriteLine($"MainPage 建構函式使用的 執行緒 {Thread.CurrentThread.ManagedThreadId}");
             Init();
+            Console.WriteLine($"MainPage 建構函式準備結束執行");
         }
 
         async Task Init()
         {
+            Console.WriteLine($"Init 進入點的 執行緒 {Thread.CurrentThread.ManagedThreadId}");
             for (int i = 0; i < 40; i++)
             {
                 gd.Rotation = (i * 10) % 360;
                 await Task.Delay(100);
             }
+            Console.WriteLine($"Init 準備離開 執行緒 {Thread.CurrentThread.ManagedThreadId}");
 
             App.Current.MainPage = new MDPage();
         }
