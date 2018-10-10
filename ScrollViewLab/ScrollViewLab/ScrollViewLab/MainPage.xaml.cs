44157b57dd1d5832aba4145b5bd7db9ec5113930
李進興 Vulcan <vulcan@miniasp.com>
2018-10-10 18:38:18 +0800

    捲動檢視版面配置 使用範例

---
+++ ScrollViewLab/ScrollViewLab/ScrollViewLab/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ScrollViewLab
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGoLeft_Clicked(object sender, EventArgs e)
+        {
+            scrollview.ScrollToAsync(-150, 0, true);
+        }
+
+        private void btnGoRight_Clicked(object sender, EventArgs e)
+        {
+            scrollview.ScrollToAsync(150, 0, true);
+        }
+
+        private void btnGoLabel_Clicked(object sender, EventArgs e)
+        {
+            scrollview.ScrollToAsync(lblHor, ScrollToPosition.Center, true);
+        }
+    }
+}
