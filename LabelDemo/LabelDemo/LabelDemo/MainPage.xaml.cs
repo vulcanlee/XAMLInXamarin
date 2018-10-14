a71dd47d54bc279d33e3c9e300919036f24d2e60
李進興 Vulcan <vulcan@miniasp.com>
2018-10-14 21:03:55 +0800

    文字標籤 使用範例

---
+++ LabelDemo/LabelDemo/LabelDemo/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LabelDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMore_Clicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new NavigationPage(new Page1());
+        }
+    }
+}
---
+++ LabelDemo/LabelDemo/LabelDemo/Page1.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LabelDemo
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Page1 : ContentPage
+	{
+		public Page1 ()
+		{
+			InitializeComponent ();
+		}
+
+        private void btnMainPage_Clicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new NavigationPage(new MainPage());
+        }
+    }
+}
