2873e5a98e8090055be881111a5d76365549996b
李進興 Vulcan <vulcan@miniasp.com>
2018-10-18 14:50:28 +0800

    按鈕與交換器

---
+++ ButtonDemo/ButtonDemo/ButtonDemo/MainPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace ButtonDemo
+{
+        public class MainPageViewModel : INotifyPropertyChanged
+        {
+            private string _CommandMessage = "";
+            public string CommandMessage
+            {
+                get { return _CommandMessage; }
+                set
+                {
+                    _CommandMessage = value;
+                    OnPropertyChanged("CommandMessage");
+                }
+            }
+            public ICommand ButtonCommand { get; set; }
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            public MainPageViewModel()
+            {
+                ButtonCommand = new Command(() =>
+                {
+                    CommandMessage = $"按鈕已經按下了 {DateTime.Now}";
+                });
+            }
+
+            protected virtual void OnPropertyChanged(string propertyName)
+            {
+                if (PropertyChanged == null)
+                    return;
+
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    public partial class MainPage : ContentPage
+    {
+        Color backupColor;
+        
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BindingContext = new MainPageViewModel();
+
+            backupColor = sw.OnColor;
+        }
+
+        private void swEnableColor_Toggled(object sender, ToggledEventArgs e)
+        {
+            if(swEnableColor.IsToggled == true)
+            {
+                sw.OnColor = Color.Gold;
+            }
+            else
+            {
+                sw.OnColor = backupColor;
+            }
+        }
+    }
+}
