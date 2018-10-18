e9a5ad94580352309f1203692054c5723a9894bb
李進興 Vulcan <vulcan@miniasp.com>
2018-10-18 19:59:50 +0800

    網頁檢視 使用範例

---
+++ WebViewDemo/WebViewDemo/WebViewDemo/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace WebViewDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var htmlWebViewSource = new HtmlWebViewSource();
+            htmlWebViewSource.Html = @"<html><body><h1>Xamarin.Forms</h1> <p>Welcome to WebView.</p>  </body></html>";
+            webview.Source = htmlWebViewSource;
+        }
+
+        private void WebView_Navigating(object sender, WebNavigatingEventArgs e)
+        {
+            labelNavigating.Text = $"Navigating time {DateTime.Now}";
+        }
+
+        private void WebView_Navigated(object sender, WebNavigatedEventArgs e)
+        {
+            labelNavigated.Text = $"Navigated time {DateTime.Now}";
+        }
+    }
+}
