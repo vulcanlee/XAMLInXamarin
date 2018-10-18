2c3a01592c15a1f8e135a61942d3f6a15c06b520
李進興 Vulcan <vulcan@miniasp.com>
2018-10-18 12:27:10 +0800

    文字輸入盒與文字編輯器 使用範例

---
+++ EntryDemo/EntryDemo/EntryDemo/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace EntryDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void entryEvent_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            labelNotify.Text = $"輸入文字長度為 {entryEvent.Text.Length}";
+            if(entryEvent.Text.Length<=10)
+            {
+                labelNotify.IsVisible = true;
+            }
+            else
+            {
+                labelNotify.IsVisible = false;
+            }
+        }
+
+        private void entryEvent_Completed(object sender, EventArgs e)
+        {
+            if (entryEvent.Text.Length <= 10)
+            {
+                DisplayAlert("錯誤", "輸入文字長度必須大於10", "確定");
+            }
+        }
+
+        private void btnEditor_Clicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new NavigationPage(new Page1());
+        }
+
+        private void entryEvent_Unfocused(object sender, FocusEventArgs e)
+        {
+            if (entryEvent.Text.Length <= 10)
+            {
+                DisplayAlert("錯誤 !!", "輸入文字長度必須大於10", "確定");
+            }
+        }
+    }
+}
---
+++ EntryDemo/EntryDemo/EntryDemo/Page1.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EntryDemo
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Page1 : ContentPage
+	{
+		public Page1 ()
+		{
+			InitializeComponent ();
+		}
+
+        private void btnMoreText_Clicked(object sender, EventArgs e)
+        {
+            editor.Text += "Xamarin.Forms 會公開適用於 .NET 開發人員"+
+                "的完整跨平台 UI 工具組。 在 Visual Studio 中使用 C#"+"" +
+                " 建置完整的原生 Android、iOS 與通用 Windows 平台應用程式。";
+        }
+
+        private void btnGoMain_Clicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new NavigationPage(new MainPage());
+        }
+
+        private void btnLessText_Clicked(object sender, EventArgs e)
+        {
+            editor.Text = "自動調整大小的文字編輯器";
+        }
+    }
+}
