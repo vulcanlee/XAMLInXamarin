f8d22b025253b8e7d2066f1d2a0a8d7115804f53
李進興 Vulcan <vulcan@miniasp.com>
2018-10-19 10:49:45 +0800

    滑桿與 使用範例

---
+++ SliderDemo/SliderDemo/SliderDemo/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SliderDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            boxview.Color = Color.FromRgba(sliderR.Value, sliderG.Value, sliderB.Value, sliderA.Value);
+        }
+
+        private void sliderA_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            boxview.Color = Color.FromRgba(sliderR.Value, sliderG.Value, sliderB.Value, sliderA.Value);
+        }
+
+        private void sliderR_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            boxview.Color = Color.FromRgba(sliderR.Value, sliderG.Value, sliderB.Value, sliderA.Value);
+        }
+
+        private void sliderG_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            boxview.Color = Color.FromRgba(sliderR.Value, sliderG.Value, sliderB.Value, sliderA.Value);
+        }
+
+        private void sliderB_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            boxview.Color = Color.FromRgba(sliderR.Value, sliderG.Value, sliderB.Value, sliderA.Value);
+        }
+    }
+}
